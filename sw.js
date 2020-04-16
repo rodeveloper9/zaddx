b3971fe90f547f6b51dee293316fbdf587489f80
Rohit <rohit@technogramsolutions.com>
2020-04-16 23:29:52 +0530

    UPDATE: PWA implemented

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Service Worker
+const cacheName = "my-first-pwa";
+const filesToCache = [
+    "/",
+    "index.html",
+    "./js/index.js",
+    "./css/style.css"
+];
+
+self.addEventListener("install", installEvent => {
+    console.log("[ServiceWorker**] Install");
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log("[ServiceWorker**] Caching app shell");
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
